54e6eca78e1fea0d27167ad9350cb828efb941f8
tiisus <97797762+tiisus@users.noreply.github.com>
2022-02-01 03:59:15 +0200

    Adding actions and other main pages

---
+++ actions.js
@@ -0,0 +1,40 @@
+/* changes page to info section*/
+function toInfo() {
+	
+	location.assign("https://tiisus.github.io/info.html");
+	
+}
+
+/* changes page to easter egg section*/
+function toExit() {
+	
+	location.assign("");
+	
+}
+
+/* changes page to portfolio section*/
+function toPortfolio(){
+	
+	location.assign("https://tiisus.github.io/display.html");
+	
+}
+
+/* changes page to developer section*/
+function toDev(){
+	
+	location.assign("https://tiisus.github.io/dev.html");
+	
+}
+
+/* transports to Theseus */
+function toTheseus() {
+	
+	location.assign("https://www.theseus.fi/handle/10024/504732");
+	
+}
+
+/* takes back to mainpage */
+function toMain(){
+
+	location.assign("https://tiisus.github.io/");
+}
